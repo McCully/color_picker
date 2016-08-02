24ff44e547f179dedbabb329e4ca9e5bb429587b
Mat McCully <mccully1894@gmail.com>
2016-08-02 18:53:45 -0500

    base

---
+++ app.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  var blocks = ["red" , "green" , "yellow" , "blue" , "black" , "purple"];
+  $("body").append('<div id = "color red"></div>');
+  $("body").append('<div id = "color green"></div>');
+  $("body").append('<div id = "color yellow"></div>');
+  $("body").append('<div id = "color blue"></div>');
+  $("body").append('<div id = "color black"></div>');
+  $("body").append('<div id = "color purple"></div>');
+
+
+
+  function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+  };
+
+  var randColor =
+
+});
