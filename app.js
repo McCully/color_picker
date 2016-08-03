5fa0224dae4a65bab6bab09dd2fa8203c8e77022
McCully <mccully1894@gmail.com>
2016-08-02 19:31:38 -0500

    almost there!

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
-  var blocks = ["red" , "green" , "yellow" , "blue" , "black" , "purple"];
-  $("body").append('<div id = "color red"></div>');
-  $("body").append('<div id = "color green"></div>');
-  $("body").append('<div id = "color yellow"></div>');
-  $("body").append('<div id = "color blue"></div>');
-  $("body").append('<div id = "color black"></div>');
-  $("body").append('<div id = "color purple"></div>');
+  alert("Brace yourselves, colors are coming")
+  $("#color").on("click" , game);
+});
+  var colors ["red" , "green" , "yellow" , "blue" , "black" , "purple"];
+  var randColor;
+  $(".color").append('<div id = "color red"></div>');
+  $(".color").append('<div id = "color green"></div>');
+  $(".color").append('<div id = "color yellow"></div>');
+  $(".color").append('<div id = "color blue"></div>');
+  $(".color").append('<div id = "color black"></div>');
+  $(".color").append('<div id = "color purple"></div>');
+
+function colorChooser(){
+var index = randomNumber(0 , 5);
+randColor = colors[index];
+}
 
 
 
@@ -13,6 +22,12 @@ $(document).ready(function() {
     return Math.floor(Math.random() * (1 + max - min) + min);
   };
 
-  var randColor =
 
-});
+function game(){
+  var thisColor = $(this).attr('id');
+  if (thisColor == randColor){
+    $('#correct').append("<p>Way to go!</p>")
+  } else {
+    $('#incorrect').append("<p>You'll get it next time!</p>")
+  }
+}
